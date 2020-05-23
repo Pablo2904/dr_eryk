040c6554b67fb9d27f0cfdf68c6450812595e64f
Pawel Koscielny <pawel.koscielny2904@gmail.com>
2020-05-23 11:26:06 +0200

    ERYK-001: Libs installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
 
 function App() {
+  const App = styled.div`
+    background-color: #282c34;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    color: white;
+  `;
+
   return (
-    <div className="App">
+    <App>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,7 +31,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </App>
   );
 }
 
